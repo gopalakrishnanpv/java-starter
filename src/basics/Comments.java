02041dae8e91aaba3b152495c9ecc1081c2ecdbd
gopalakrishnanpv <gopalakrishnan1505@gmail.com>
2021-07-09 19:15:13 +0530

    java starter files

---
+++ src/basics/Comments.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Comments {
+    public void showComments() {
+        // This is a single line comment
+/*        This is
+                a multi line comment*/
+        System.out.println("The single and multi line comment above will not be executed!");
+    }
+}
---
+++ src/main/JavaStarter.java
@@ -0,0 +1,11 @@
+package main;
+
+import basics.Comments;
+
+public class JavaStarter {
+    public static void main(String[] args) {
+        Comments comments = new Comments();
+        comments.showComments();
+
+    }
+}
